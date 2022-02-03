6e95233a2e7fe4dd92e576d611100ca2b0fa8e6f
gowthamSolulab <gowtham@solulab.com>
2022-02-03 23:31:11 +0530

    user route added

--- src/api/components/index.js
+++ src/api/components/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import admin from './admin/adminRoute.js';
+import user from './user/userRoute.js';
 
 const router = express.Router();
 
 router.use('/admin', admin);
+router.use('/user', user);
 
 export default router;
