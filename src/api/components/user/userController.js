30219488ac781b565627c600eb7bdc41298e2529
gowthamSolulab <gowtham@solulab.com>
2022-02-04 17:20:55 +0530

    userLogin getWalletBalance methods added. signUp, getUserProfile and userUpdateProfile updated

--- src/api/components/user/userController.js
+++ src/api/components/user/userController.js
@@ -1,26 +1,37 @@
-import { createUser, getUser, updateUser } from './userService.js';
+import {
+	createUser,
+	getUser,
+	updateUser,
+	login,
+	getBalance,
+} from './userService.js';
 import { handleResponse, handleError } from '../../helpers/responseHandler.js';
 import logger from '../../config/logger.js';
 
 export const signUp = async (req, res) => {
 	try {
-		const user = await createUser(req.body);
-		if (!user) return handleError(res, 400, 'something went wrong');
+		const data = await createUser(req.body);
+		if (data.err_msg)
+			return handleError({
+				res,
+				statusCode: 400,
+				err_msg: data.err_msg,
+			});
 		return handleResponse({
 			res,
-			statusCode: 200,
+			statusCode: 201,
 			msg: 'User signup success',
-			user,
+			data,
 		});
 	} catch (err) {
-		logger.info(err);
+		logger.info(err.message);
 	}
 };
 
 export const getUserProfile = async (req, res) => {
 	try {
-		const userData = await getUser(req.params.id);
-		if (!userData)
+		const data = await getUser(req.params.id);
+		if (!data)
 			return handleError({
 				res,
 				statusCode: 401,
@@ -30,29 +41,73 @@ export const getUserProfile = async (req, res) => {
 			res,
 			statusCode: 200,
 			msg: 'Profile fetched successfully',
-			userData,
+			data,
 		});
 	} catch (err) {
-		logger.info(err);
+		logger.info(err.message);
 	}
 };
 
 export const updateUserProfile = async (req, res) => {
 	try {
-		const updatedUser = await updateUser(req.params.id, req.body);
-		if (!updatedUser)
+		const data = await updateUser(req.params.id, req.body);
+		if (data.err_msg)
 			return handleError({
 				res,
-				statusCode: 400,
-				err_msg: `something went wrong`,
+				statusCode: 401,
+				err_msg: data.err_msg,
 			});
 		return handleResponse({
 			res,
 			statusCode: 200,
 			msg: 'Profile updated successfully',
-			userData,
+			data,
 		});
 	} catch (err) {
-		logger.info(err);
+		logger.info(err.message);
+	}
+};
+
+export const userLogin = async (req, res) => {
+	try {
+		const data = await login(req.body);
+		if (data.err_msg)
+			return handleError({
+				res,
+				statusCode: 401,
+				err_msg: data.err_msg,
+			});
+
+		const { token, cookieOptions, user } = data;
+		res.cookie('jwt', token, cookieOptions);
+
+		return handleResponse({
+			res,
+			statusCode: 200,
+			msg: 'Logged in  successfully',
+			data: { user, token },
+		});
+	} catch (err) {
+		logger.info(err.message);
+	}
+};
+
+export const getWalletBalance = async (req, res) => {
+	try {
+		const data = await getBalance(req.params.id);
+		if (data.err_msg)
+			return handleError({
+				res,
+				statusCode: 401,
+				err_msg: data.err_msg,
+			});
+		return handleResponse({
+			res,
+			statusCode: 200,
+			msg: 'User Wallet Balance',
+			data,
+		});
+	} catch (err) {
+		logger.info(err.message);
 	}
 };
