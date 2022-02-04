e42f4095de55f26a6a5e9f0fe4ff63f322e88a3f
gowthamSolulab <gowtham@solulab.com>
2022-02-04 17:31:26 +0530

    login and walletBalance routes added, validators added for signup and updateProfile

--- src/api/components/user/userRoute.js
+++ src/api/components/user/userRoute.js
@@ -1,10 +1,22 @@
 import express from 'express';
-import { signUp, getUserProfile, updateUserProfile } from './userController.js';
+import {
+	signUp,
+	getUserProfile,
+	updateUserProfile,
+	getWalletBalance,
+	userLogin,
+} from './userController.js';
+import {
+	validateSignup,
+	validateUpdateUserProfile,
+} from '../../middleware/validator.js';
 
 const router = express.Router();
 
-router.post('/signup', signUp);
+router.post('/signup', validateSignup, signUp);
 router.get('/userProfile/:id', getUserProfile);
-router.post('/updateProfile/:id', updateUserProfile);
+router.post('/login', userLogin);
+router.post('/updateProfile/:id', validateUpdateUserProfile, updateUserProfile);
+router.get('/walletBalance/:id', getWalletBalance);
 
 export default router;
