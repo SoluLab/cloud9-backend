20a0bd5fc1836dec70a9a6c42adcad425a6a1a33
gowthamSolulab <gowtham@solulab.com>
2022-02-04 17:22:10 +0530

    confirmPassword removed

--- src/api/components/user/userModal.js
+++ src/api/components/user/userModal.js
@@ -1,7 +1,4 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-// eslint-disable-next-line no-unused-vars
-import validator from 'validator';
 
 const userSchema = new mongoose.Schema(
 	{
@@ -10,32 +7,21 @@ const userSchema = new mongoose.Schema(
 			unique: true,
 			required: true,
 		},
+		firstName: String,
+		lastName: String,
+		userName: String,
 		password: {
 			type: String,
 			required: true,
+			select: false,
 		},
-		confirmPassword: {
-			type: String,
-			required: [true, 'Please confirm your password'],
-			validate: {
-				validator(el) {
-					return el === this.password;
-				},
-				message: 'Passwords are not the same!',
-			},
-		},
+		wallet: String,
 	},
+
 	{
 		timestamps: true,
 	}
 );
-
-userSchema.pre('save', async function (next) {
-	this.password = await bcrypt.hash(this.password, 12);
-	// Delete confirmPassword field
-	this.confirmPassword = undefined;
-	next();
-});
 
 const User = mongoose.model('User', userSchema);
 
