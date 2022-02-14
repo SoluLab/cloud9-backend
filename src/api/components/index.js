228ca4145ac0013a577a822f1284defcb8e7fb8b
gowthamSolulab <gowtham@solulab.com>
2022-02-14 18:13:24 +0530

    notification route  added

--- src/api/components/index.js
+++ src/api/components/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import admin from './admin/adminRoute.js';
 import user from './user/userRoute.js';
+import notification from './notification/notificationRoute.js';
 
 const router = express.Router();
 
 router.use('/admin', admin);
 router.use('/user', user);
+router.use('/notification', notification);
 
 export default router;
