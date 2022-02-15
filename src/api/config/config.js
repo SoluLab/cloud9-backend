e7c99090fb231dc3b81e499f36dc7e9909bedc38
Parth2412 <kaloliya@gmail.com>
2022-02-15 23:51:46 +0530

    changed the config file

--- src/api/config/config.js
+++ src/api/config/config.js
@@ -19,8 +19,14 @@ export default {
 		},
 	},
 	alchemyUrl: process.env.ALCHEMY_URL,
+	gasPriceEstimateUrl: process.env.MATIC_GAS_STATION,
+	etherscanApiKey: process.env.ETHER_SCAN_API_KEY,
 	contracts: {
 		tokenContract: process.env.CLOUD9_TOKEN_CONTRACT_ADDRESS,
 		icoContract: process.env.CLOUD9_ICO_CONTRACT_ADDRESS,
+	},
+	contractAccounts: {
+		deploymentAddress: process.env.DEPLOYMENT_ADDRESS,
+		deploymentPrivateKey: process.env.DEPLOYMENT_PRIVATE_KEY,
 	},
 };
