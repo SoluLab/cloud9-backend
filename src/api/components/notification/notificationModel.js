4fefd5ebbf849307948a4bcab525a5fc79482b9b
gowthamSolulab <gowtham@solulab.com>
2022-02-16 21:30:45 +0530

    isRead field added

--- src/api/components/notification/notificationModel.js
+++ src/api/components/notification/notificationModel.js
@@ -12,6 +12,7 @@ const notificationSchema = new mongoose.Schema({
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'User',
 	},
+	isRead: { type: Boolean, default: false },
 	createdAt: {
 		type: Date,
 		default: Date.now(),
