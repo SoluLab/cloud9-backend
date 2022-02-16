1309c30a25355e6b88cb68d22b5ad58d4806d8bf
gowthamSolulab <gowtham@solulab.com>
2022-02-16 15:37:10 +0530

    middleware added

--- src/api/components/notification/notificationRoute.js
+++ src/api/components/notification/notificationRoute.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { getUserNotifications } from './notificationController.js';
-
+import { isLoggedIn } from '../user/userController.js';
 const router = express.Router();
 
-router.get('/getNotifications', getUserNotifications);
+router.get('/getNotifications', isLoggedIn, getUserNotifications);
 
 export default router;
