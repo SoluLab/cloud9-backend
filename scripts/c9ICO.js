2c4b05495fb872d075cc62b3e88f4e6efdc2a786
Parth2412 <kaloliya@gmail.com>
2022-02-22 23:39:24 +0530

    Script changed

--- scripts/c9ICO.js
+++ scripts/c9ICO.js
@@ -3,7 +3,11 @@ const hre = require('hardhat');
 
 async function main() {
 	const Cloud9ICO = await hre.ethers.getContractFactory('CloudNineICO');
-	const cloud9ICO = await Cloud9ICO.deploy();
+	const cloud9ICO = await Cloud9ICO.deploy(
+		'1',
+		'0x3941Fa693608240B82B241670Be1e301a7871c94',
+		'0xe87546503256d704Bb59345Fe0DE5a033459C312'
+	);
 
 	await cloud9ICO.deployed();
 
