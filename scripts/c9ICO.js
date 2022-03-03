8ea8c6cd4cf614827e87c966158a9e7cb1a65cbd
Parth2412 <kaloliya@gmail.com>
2022-03-03 14:30:55 +0530

    importing env variables

--- scripts/c9ICO.js
+++ scripts/c9ICO.js
@@ -1,12 +1,14 @@
 // eslint-disable-next-line no-undef
+require('dotenv').config();
+
 const hre = require('hardhat');
 
 async function main() {
 	const Cloud9ICO = await hre.ethers.getContractFactory('CloudNineICO');
 	const cloud9ICO = await Cloud9ICO.deploy(
 		'1',
-		'0x3941Fa693608240B82B241670Be1e301a7871c94',
-		'0xF4D5B20e565a8834d63be5672fC4Fc769d0Aa2E6'
+		process.env.DEPLOYMENT_ADDRESS,
+		process.env.CLOUD9_TOKEN_CONTRACT_ADDRESS
 	);
 
 	await cloud9ICO.deployed();
