61c1964d1daf0da2ea1199ea4ea41c6718b88061
gowthamSolulab <gowtham@solulab.com>
2022-03-04 16:31:55 +0530

    subscribe route added

--- src/api/components/index.js
+++ src/api/components/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import user from './user/userRoute.js';
 import admin from './admin/adminRoute.js';
 import notification from './notification/notificationRoute.js';
+import subscribe from './subscription/subscriptionRoute.js';
 
 const router = express.Router();
 
 router.use('/user', user);
 router.use('/notification', notification);
 router.use('/admin', admin);
+router.use('/subscribe', subscribe);
 
 export default router;
