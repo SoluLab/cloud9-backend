ce6e4944c02274cd4e3d8adac41b3ffe4cb6c6d2
gowthamSolulab <gowtham@solulab.com>
2022-03-04 17:33:30 +0530

    sendMail helper created

---
+++ src/api/helpers/sendEmail.js
@@ -0,0 +1,20 @@
+import sgMail from '@sendgrid/mail';
+import logger from '../config/logger.js';
+
+import { default as config } from '../config/config.js';
+
+export const sendMail = async (recipient, data) => {
+	try {
+		logger.info('Inside sendMail helper');
+		sgMail.setApiKey(config.sendGrid.apiKey);
+		const msg = {
+			to: recipient,
+			from: config.sendGrid.sender,
+			subject: data.subject,
+			text: data.text,
+		};
+		return await sgMail.send(msg);
+	} catch (error) {
+		logger.info(error.response.body.errors[0].message);
+	}
+};
