0b202c6243c112e3bc64a8a351d26a3b8ef8f61b
gowthamSolulab <gowtham@solulab.com>
2022-03-07 20:30:03 +0530

    forgotPasswordService and resetPasswordService added

--- src/api/components/user/userService.js
+++ src/api/components/user/userService.js
@@ -13,6 +13,7 @@ import { default as config } from '../../config/config.js';
 import geoip from 'geoip-lite';
 import logger from '../../config/logger.js';
 import Aws from 'aws-sdk';
+import { sendMail } from '../../helpers/sendEmail.js';
 
 const web3 = new Web3(new Web3.providers.HttpProvider(config.alchemyUrl));
 const contract = new web3.eth.Contract(
@@ -436,6 +437,125 @@ export const getTokenSaleProcessService = async () => {
 			'Available Balance': Number(holderAvailableBalanceInDecimal.toFixed(4)),
 			'Amount Spent': Number(holderSpentBalance.toFixed(4)),
 		};
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const forgotPasswordService = async (email) => {
+	try {
+		logger.info('Inside forgotPassword Service');
+		let user = await User.findOne({ email }, 'email firstName lastName');
+		if (!user)
+			return {
+				err_msg: `User doesn't exist with this email please signUp`,
+				statusCode: 401,
+			};
+		const token = jwt.sign(
+			{ id: user._id, email: user.email },
+			config.jwtSecret,
+			{
+				expiresIn: 30 * 60 * 1000,
+			}
+		);
+		const resetLink = `url?token=${token}`;
+		const data = await sendMail(email, {
+			subject: 'Reset Password',
+			html: `
+					<!doctype html>
+					<html lang="en-US">
+
+					<head>
+							<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+							<title>Reset Password Email Template</title>
+							<meta name="description" content="Reset Password Email Template.">
+							<style type="text/css">
+									a:hover {text-decoration: underline !important;}
+							</style>
+					</head>
+
+					<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+							<!--100% body table-->
+							<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+									style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+									<tr>
+											<td>
+													<table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+															align="center" cellpadding="0" cellspacing="0">
+															<tr>
+																	<td style="height:80px;">&nbsp;</td>
+															</tr>
+															<tr>
+																	<td style="height:20px;">&nbsp;</td>
+															</tr>
+															<tr>
+																	<td>
+																			<table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+																					style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+																					<tr>
+																							<td style="height:40px;">&nbsp;</td>
+																					</tr>
+																					<tr>
+																							<td style="padding:0 35px;">
+																									<h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
+																											requested to reset your password</h1>
+																									<span
+																											style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+																									<p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+																											A unique link to reset your
+																											password has been generated for you. To reset your password, click the
+																											following link and follow the instructions.
+																									</p>
+																									<a href="${resetLink}"
+																											style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
+																											Password</a>
+																							</td>
+																					</tr>
+																					<tr>
+																							<td style="height:40px;">&nbsp;</td>
+																					</tr>
+																			</table>
+																	</td>
+															<tr>
+																	<td style="height:20px;">&nbsp;</td>
+															</tr>
+															<tr>
+																	<td style="text-align:center;">
+																			<p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.cloud9.cool </strong></p>
+																	</td>
+															</tr>
+															<tr>
+																	<td style="height:80px;">&nbsp;</td>
+															</tr>
+													</table>
+											</td>
+									</tr>
+							</table>
+							<!--/100% body table-->
+					</body>
+
+					</html>`,
+		});
+		if (data && data[0].statusCode === 202) return { email };
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const resetPasswordService = async (token, newPassword) => {
+	try {
+		const verifiedToken = await promisify(jwt.verify)(token, config.jwtSecret);
+		const user = await User.findOne({ email: verifiedToken.email }).select(
+			'+password'
+		);
+		user.password = await hashPassword(newPassword);
+		const data = await user.save();
+		if (!data)
+			return {
+				err_msg: 'Something went wrong please try again',
+				statusCode: 400,
+			};
+		return data;
 	} catch (error) {
 		throw error;
 	}
